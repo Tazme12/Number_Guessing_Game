4530587456e61c75bc9897c2e3166388e7b595c8
Tazme12 <camthornton.07@gmail.com>
2025-04-29 13:28:48 +0100

    menueasy_completed

--- number_guessing_game/backend.py
+++ number_guessing_game/backend.py
@@ -1,5 +1,11 @@
 import random
 
+def playagain():
+    print("Would you like to play again?")
+    pa_input = input("Enter Y/N: ").upper()
+    if pa_input == Y:
+        print("Choice accepted")
+
 def game_rules():
     print ("1. Easy (10 chances)")
     print ("2. Medium (5 chances)")
@@ -8,22 +14,42 @@ def game_rules():
     if optionselect == "EASY":
         print("Great! You have selected the Easy difficulty level.")
         print("Let's start the game!")
+        print ("I'm thinking of a number between 1 and 100.")
         menueasy()
 
 
 def menueasy():
-    while True:
-        for i in range(10):
-            number = random.randint(1, 100)
+    guesses = 10
+    attempts = 0
+    number = random.randint(1, 100)
+    while guesses > 0:    
+        try:
+            print(number)
             userinput = int(input("Please enter a number to guess: "))
             if userinput == number:
-                print("Congratulations, you guessed correctly")                
-            elif userinput != number:
-                print("Incorrect, you have", i ,"guesses left")
-            else:
-                print("Please enter a number: ")
+                guesses -= 1
+                print("Congratulations, you guessed correctly")
+                attempts += 1
+                print("You took", attempts,"attempts")
+                print("Would you like to play again?")
+                pa_input = input("Enter Y/N: ").upper()
+                if pa_input == 'Y':
+                    continue
+                else:
+                    break
+            elif userinput > number:
+                print("Incorrect, lower")
+                guesses -= 1
+                print("You have", guesses,"guesses left")
+                attempts += 1
+            elif userinput < number:
+                print("Incorrect, higher")
+                guesses -= 1
+                print("You have", guesses,"guesses left")
+                attempts += 1
+        except ValueError:
+            print("Thats not a number, Try again.")
 
 print ("Welcome to the Number Guessing Game!")
 print ("Below are the difficulty levels for the game:")
 game_rules()
-print ("I'm thinking of a number between 1 and 100.")
